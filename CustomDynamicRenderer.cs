05421e6fc8c63add560dedbdeb18c1ce25ebd0be
Jaygem <youness.kafia@devinci.fr>
2018-07-29 21:10:56 +0200

    Initial commit

---
+++ CustomDynamicRenderer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Input.StylusPlugIns;
+using System.Windows.Input;
+using System.Windows.Ink;
+
+namespace AnIBmator
+{
+    // A StylusPlugin that renders ink with a linear gradient brush effect.
+    class CustomDynamicRenderer : DynamicRenderer
+    {
+        [ThreadStatic]
+        static private Brush brush = null;
+
+        [ThreadStatic]
+        static private Pen pen = null;
+
+        private Point prevPoint;
+
+        protected override void OnStylusDown(RawStylusInput rawStylusInput)
+        {
+            // Allocate memory to store the previous point to draw from.
+            prevPoint = new Point(double.NegativeInfinity, double.NegativeInfinity);
+            base.OnStylusDown(rawStylusInput);
+        }
+
+        protected override void OnDraw(DrawingContext drawingContext,
+                                       StylusPointCollection stylusPoints,
+                                       Geometry geometry, Brush fillBrush)
+        {
+            // Create a new Brush, if necessary.
+            if (brush == null)
+            {
+                brush = new LinearGradientBrush(Colors.Red, Colors.Blue, 20d);
+            }
+
+            // Create a new Pen, if necessary.
+            if (pen == null)
+            {
+                pen = new Pen(brush, 2d);
+            }
+
+            // Draw linear gradient ellipses between 
+            // all the StylusPoints that have come in.
+            for (int i = 0; i < stylusPoints.Count; i++)
+            {
+                Point pt = (Point)stylusPoints[i];
+                Vector v = Point.Subtract(prevPoint, pt);
+
+                // Only draw if we are at least 4 units away 
+                // from the end of the last ellipse. Otherwise, 
+                // we're just redrawing and wasting cycles.
+                if (v.Length > 4)
+                {
+                    // Set the thickness of the stroke based 
+                    // on how hard the user pressed.
+                    double radius = stylusPoints[i].PressureFactor * 10d;
+                    drawingContext.DrawEllipse(brush, pen, pt, radius, radius);
+                    prevPoint = pt;
+                }
+            }
+        }
+    }
+}
